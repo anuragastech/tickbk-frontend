7a687a8c3857c1f1022ab7782f4d7dbf17d1bd84
Anurag <anuragraveendren98@gmail.com>
2024-12-11 02:16:51 +0530

    updated tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/user/login/Login"; // Replace with your actual components
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/user/login/Login";
 import Signup from "./components/user/signup/signup";
 import Events from "./components/user/events/Events";
-import AddEvent from "./components/client/addEevnt";
+// import AddEvent from "./components/client/addEvent";
 import About from "./components/user/about/about";
 import Profile from "./components/user/profile/profile";
 import Cart from "./components/user/cart/cart";
@@ -13,18 +12,21 @@ import AdminSignup from "./components/client/adminSignup";
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} /> {/* Default route */}
-        <Route path="/adminLogin" element={<AdminLogin />} />
-        <Route path="/adminSignup" element={<AdminSignup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/addEvent" element={<AddEvent />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <div>
+        <Routes>
+          {/* Redirect root ("/") to "/login" */}
+          {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+          <Route path="/adminLogin" element={<AdminLogin />} />
+          <Route path="/adminSignup" element={<AdminSignup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/events" element={<Events />} />
+          {/* <Route path="/addEvent" element={<AddEvent />} /> */}
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
